f8a2d954fbcd239dc5e76ae4577b742b2a118c82
Mankind <mankindofficial1@gmail.com>
2022-04-22 00:10:01 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import AppProvider from "./AppProvider";
+import Header from "./components/Header/Header";
+import Main from "./components/Body/Main";
+import Footer from "./components/Footer/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <AppProvider>
+        <div className="app">
+          <Header />
+          <Main />
+          <Footer />
+        </div>
+      </AppProvider>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./Footer.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <a
+          href="https://musliudeen-makinde.netlify.app"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Created by Musliudeen Makinde
+        </a>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
